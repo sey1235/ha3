186cb4ae46f247d8866f907e9cc1f502abed8859
kanika-sehgal123 <147759163+kanika-sehgal123@users.noreply.github.com>
2024-10-19 17:30:40 +0200

    Add files via upload

---
+++ src/test/java/com/github/fhdo7100003/ha/LoggerTest.java
@@ -0,0 +1,14 @@
+package com.github.fhdo7100003.ha;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoggerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
