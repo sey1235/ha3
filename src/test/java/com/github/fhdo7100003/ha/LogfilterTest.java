df648c413b564996ef0b955ddd9f96a74cf7af61
kanika-sehgal123 <147759163+kanika-sehgal123@users.noreply.github.com>
2024-10-19 12:50:50 +0200

    Add files via upload

---
+++ src/test/java/com/github/fhdo7100003/ha/LogfilterTest.java
@@ -0,0 +1,14 @@
+package com.github.fhdo7100003.ha;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LogfilterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
