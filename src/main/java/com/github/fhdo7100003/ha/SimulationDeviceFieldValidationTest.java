ccb3bd7a236dac6653472687a83dc5fec2ee6f1d
sey1235 <82286005+sey1235@users.noreply.github.com>
2024-11-07 10:12:17 +0100

    Create SimulationDeviceFieldValidationTest.java

---
+++ src/main/java/com/github/fhdo7100003/ha/SimulationDeviceFieldValidationTest.java
@@ -0,0 +1,47 @@
+package ha;
+
+import java.io.*;
+import java.nio.file.Files;
+
+class DeviceLoggingTest {
+    private final File logFile;
+
+    public DeviceLoggingTest() throws IOException {
+        logFile = new File(System.getProperty("java.io.tmpdir"), "device_log.txt");
+        if (!logFile.exists()) logFile.createNewFile();
+    }
+
+    public void logEnergy(int amount, boolean isProducing) throws IOException {
+        try (FileWriter writer = new FileWriter(logFile, true)) {
+            if (isProducing) {
+                writer.write("Produced " + amount + " units of energy\n");
+            } else {
+                writer.write("Consumed " + amount + " units of energy\n");
+            }
+        }
+        System.out.println("Logged " + amount + " units of energy.");
+    }
+
+    public void printLogContents() throws IOException {
+        System.out.println("Log contents:");
+        Files.readAllLines(logFile.toPath()).forEach(System.out::println);
+    }
+
+    public void cleanup() {
+        if (logFile.exists()) logFile.delete();
+    }
+
+    public static void main(String[] args) {
+        try {
+            DeviceLoggingTest test = new DeviceLoggingTest();
+            test.logEnergy(150, true);
+            test.logEnergy(100, false);
+
+            test.printLogContents();
+
+            test.cleanup();
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
